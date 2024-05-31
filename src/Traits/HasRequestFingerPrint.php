ac8e5e830ef06f7b63d97d181a4836dc6098affe
kay briyel <kaybriyel@gmail.com>
2024-05-31 17:13:41 +0700

    add setAppFingerPrint method

--- src/Traits/HasRequestFingerPrint.php
+++ src/Traits/HasRequestFingerPrint.php
@@ -3,6 +3,7 @@
 namespace MPWT\Exceptions\Traits;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 use MPWT\Exceptions\RequestFingerPrint;
 
 trait HasRequestFingerPrint
@@ -22,4 +23,21 @@ trait HasRequestFingerPrint
         return $generator->unique($request);
     }
 
+    /**
+     * Set finger print on app
+     * 
+     * @return void
+     */
+    public function setAppFingerPrint(Request $request)
+    {
+        if (app()->offsetExists('finger_print')) return;
+
+        $finger_print = $this->getFingerPrint($request);
+
+        app()->finger_print = $finger_print;
+
+        Log::withContext([
+            'context_id' => $finger_print
+        ]);
+    }
 }
